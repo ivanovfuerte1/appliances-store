ca56b74c2999fc20da0d6191111fa89bb5af5b9e
Svetlosar Kovatchev <ivanovfuerte@yahoo.es>
2018-03-18 20:49:15 +0200

    Initial commit

---
+++ src/main/java/com/learning/appliancesstore/AppliancesStoreApplication.java
@@ -0,0 +1,12 @@
+package com.learning.appliancesstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppliancesStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppliancesStoreApplication.class, args);
+	}
+}
---
+++ src/main/java/com/learning/appliancesstore/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.learning.appliancesstore.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Created by ivanov on 18.3.2018 г..
+ */
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index(){
+        return "home/index";
+    }
+}
